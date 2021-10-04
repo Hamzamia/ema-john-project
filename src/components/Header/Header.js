200eb1a0e341da948a6291e7519eb539a86501cf
Hamzammia <mahmudul.web.developer@gmail.com>
2021-10-04 22:12:58 +0600

    add

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import { getByAltText } from '@testing-library/dom';
+import React from 'react';
+import logo from '../../images/logo.png'
+const Header = () => {
+    return (
+        <div>
+            <h2>This is header</h2>
+            <img src={logo} alt="" />
+        </div>
+    );
+};
+
+export default Header;
