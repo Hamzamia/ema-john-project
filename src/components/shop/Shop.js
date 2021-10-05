b9a95348fbd7e9cac9ad0d52abffd37b8bdf4b07
Hamzammia <mahmudul.web.developer@gmail.com>
2021-10-05 21:12:37 +0600

    hjgdf

--- src/components/shop/Shop.js
+++ src/components/shop/Shop.js
@@ -3,24 +3,34 @@ import Product from '../products/Product';
 import './Shop.css'
 
 const Shop = () => {
-    const [products, setProducts] = useState([])
+    const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
 
     useEffect(() => {
         fetch('./products.JSON')
             .then(res => res.json())
             .then(data => setProducts(data))
     }, [])
+    const handleAddToCart = (product) => {
+        const newCart = [...cart, product]
+        setCart(newCart);
+    }
+
     return (
         <div className="shop-container">
             <div className="product-container">
                 <h2>Products: {products.length}</h2>
                 {
-                    products.map(product => <Product product={product}></Product>)
+                    products.map(product => <Product
+                        product={product}
+                        handleAddToCart={handleAddToCart}
+                    >
+                    </Product>)
                 }
             </div>
             <div className="cart-container">
                 <h3>Order Summery</h3>
-                <h5>Item Order</h5>
+                <h5>Item Order: {cart.length}</h5>
             </div>
 
         </div>
